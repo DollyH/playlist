254748779cea0e5ce04c5369166f23967b5df73a
Dolly <dolly.harchandani@morganstanley.com>
2021-05-06 05:18:09 +0000

    add images and song api

--- src/main/java/com/spotify/playlist/domain/Song.java
+++ src/main/java/com/spotify/playlist/domain/Song.java
@@ -12,11 +12,13 @@ public class Song {
     private String name;
     private List<String> artists;
     private String genre;
+    private String image;
 
-    public Song(Integer id, String name, List<String> artists, String genre) {
+    public Song(Integer id, String name, List<String> artists, String genre, String image) {
         this.id = id;
         this.name = name;
         this.artists = artists;
         this.genre = genre;
+        this.image = image;
     }
 }
--- src/main/java/com/spotify/playlist/repository/SpotifyRepository.java
+++ src/main/java/com/spotify/playlist/repository/SpotifyRepository.java
@@ -2,10 +2,13 @@ package com.spotify.playlist.repository;
 
 import com.spotify.playlist.domain.Playlist;
 import com.spotify.playlist.domain.Song;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -15,18 +18,35 @@ public class SpotifyRepository {
     private final List<Playlist> playlists;
     private final List<Song> songs;
 
-    public SpotifyRepository() {
-        Song song1 = new Song(1, "Dynamite", Arrays.asList("Jin", "Suga", "RM", "J-Hope", "Jimin", "V", "Jungkook"), "Cheer");
-        Song song2 = new Song(2, "Filter", Arrays.asList("Jimin"), "Fun");
-        Song song3 = new Song(3, "Sweet Night", Arrays.asList("V"), "Soothing");
-        Song song4 = new Song(4, "Telepathy", Arrays.asList("Jin", "Suga", "RM", "J-Hope", "Jimin", "V", "Jungkook"), "Love");
-        Song song5 = new Song(5, "Just the way you are", Arrays.asList("Bruno Mars"), "Love");
-        Song song6 = new Song(6, "Spring Day", Arrays.asList("Jin", "Suga", "RM", "J-Hope", "Jimin", "V", "Jungkook"), "Love");
+    public SpotifyRepository() throws IOException {
+        byte[] fileContent = FileUtils.readFileToByteArray(ResourceUtils.getFile("classpath:images/bts-dynamite.jpg"));
+        String image = Base64.getEncoder().encodeToString(fileContent);
+        Song song1 = new Song(1, "Dynamite", Arrays.asList("Jin", "Suga", "RM", "J-Hope", "Jimin", "V", "Jungkook"), "Cheer", image);
+        fileContent = FileUtils.readFileToByteArray(ResourceUtils.getFile("classpath:images/bts-filter.jpeg"));
+        image = Base64.getEncoder().encodeToString(fileContent);
+        Song song2 = new Song(2, "Filter", Arrays.asList("Jimin"), "Fun", image);
+        fileContent = FileUtils.readFileToByteArray(ResourceUtils.getFile("classpath:images/bts-sweetnight.jpg"));
+        image = Base64.getEncoder().encodeToString(fileContent);
+        Song song3 = new Song(3, "Sweet Night", Arrays.asList("V"), "Soothing", image);
+        fileContent = FileUtils.readFileToByteArray(ResourceUtils.getFile("classpath:images/bts-telepathy.jpg"));
+        image = Base64.getEncoder().encodeToString(fileContent);
+        Song song4 = new Song(4, "Telepathy", Arrays.asList("Jin", "Suga", "RM", "J-Hope", "Jimin", "V", "Jungkook"), "Love", image);
+        fileContent = FileUtils.readFileToByteArray(ResourceUtils.getFile("classpath:images/bm-just.jpg"));
+        image = Base64.getEncoder().encodeToString(fileContent);
+        Song song5 = new Song(5, "Just the way you are", Arrays.asList("Bruno Mars"), "Love", image);
+        fileContent = FileUtils.readFileToByteArray(ResourceUtils.getFile("classpath:images/bts-springday.jpg"));
+        image = Base64.getEncoder().encodeToString(fileContent);
+        Song song6 = new Song(6, "Spring Day", Arrays.asList("Jin", "Suga", "RM", "J-Hope", "Jimin", "V", "Jungkook"), "Love", image);
+        fileContent = FileUtils.readFileToByteArray(ResourceUtils.getFile("classpath:images/bts-idol.jpg"));
+        image = Base64.getEncoder().encodeToString(fileContent);
+        Song song7 = new Song(7, "Idol", Arrays.asList("Jin", "Suga", "RM", "J-Hope", "Jimin", "V", "Jungkook"), "Cheer", image);
         Set<Song> btsSongs = new HashSet<>();
         btsSongs.add(song1);
         btsSongs.add(song2);
         btsSongs.add(song3);
         btsSongs.add(song4);
+        btsSongs.add(song6);
+        btsSongs.add(song7);
         Set<Song> slowSongs = new HashSet<>();
         slowSongs.add(song3);
         slowSongs.add(song5);
@@ -48,6 +68,7 @@ public class SpotifyRepository {
         songs.add(song4);
         songs.add(song5);
         songs.add(song6);
+        songs.add(song7);
     }
 
     public Mono<Playlist> findPlaylistByName(String name) {
